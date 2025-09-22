771c2ea923294af4318dd3d57d634426268bde39
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2025-09-22 22:30:22 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,26 +3,42 @@ const tvData = {
         {
             name: "DD News",
             logo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/DD_News.svg/1200px-DD_News.svg.png",
-            stream_url: "https://cdn-6.pishow.tv/live/13/13_2.m3u8"
+            stream_url: "https://ddnewslive.akamaized.net/hls/live/2021703/ddnews/ddnews.m3u8",
+            category: "News" // <-- Nayi Property
         },
         {
             name: "India TV",
             logo: "https://upload.wikimedia.org/wikipedia/commons/f/f4/India-tv-logo.png",
-            stream_url: "https://indiatv.live247stream.com/indiatv/live/playlist.m3u8"
+            stream_url: "https://indiatv.live247stream.com/indiatv/live/playlist.m3u8",
+            category: "News" // <-- Nayi Property
+        },
+       {
+            name: "B4U Kadak",
+            logo: "https://jiotvimages.cdn.jio.com/dare_images/images/B4U_Kadak.png",
+            stream_url: "https://cdnb4u.wiseplayout.com/B4U_Kadak/SD216/SD216.m3u8",
+            category: "Movies" // <-- Nayi Property
+        },
+       {
+            name: "9xm",
+            logo: "https://jiotvimages.cdn.jio.com/dare_images/images/9XM.png",
+            stream_url: "https://d35j504z0x2vu2.cloudfront.net/v1/manifest/0bc8e8376bd8417a1b6761138aa41c26c7309312/9xm/23886666-8fc5-470f-aab1-bd637ed607b1/3.m3u8",
+            category: "Music" // <-- Nayi Property
         }
-    ],
+   ],
     "USA": [
         {
             name: "NASA TV",
             logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png",
-            stream_url: "https://uplynkcontent.sinclairstoryline.com/channel/ddd76fdc1c0a456ba5394f71313f5963.m3u8"
+            stream_url: "https://uplynkcontent.sinclairstoryline.com/channel/ddd76fdc1c0a456ba5394f71313f5963.m3u8",
+            category: "Entertainment" // <-- Nayi Property
         }
     ],
     "International": [
         {
             name: "Al Jazeera",
             logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Aljazeera_eng.svg/512px-Aljazeera_eng.svg.png",
-            stream_url: "https://live-hls-web-aje.getaj.net/AJE/01.m3u8"
+            stream_url: "https://live-hls-web-aje.getaj.net/AJE/01.m3u8",
+            category: "News" // <-- Nayi Property
         }
     ]
 };
