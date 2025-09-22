78b7ba7c528eb9e42b9c0eec05078a6b813f68a1
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2025-09-22 22:17:16 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+const tvData = {
+    "India": [
+        {
+            name: "DD News",
+            logo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/DD_News.svg/1200px-DD_News.svg.png",
+            stream_url: "https://cdn-6.pishow.tv/live/13/13_2.m3u8"
+        },
+        {
+            name: "India TV",
+            logo: "https://upload.wikimedia.org/wikipedia/commons/f/f4/India-tv-logo.png",
+            stream_url: "https://indiatv.live247stream.com/indiatv/live/playlist.m3u8"
+        }
+    ],
+    "USA": [
+        {
+            name: "NASA TV",
+            logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png",
+            stream_url: "https://uplynkcontent.sinclairstoryline.com/channel/ddd76fdc1c0a456ba5394f71313f5963.m3u8"
+        }
+    ],
+    "International": [
+        {
+            name: "Al Jazeera",
+            logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Aljazeera_eng.svg/512px-Aljazeera_eng.svg.png",
+            stream_url: "https://live-hls-web-aje.getaj.net/AJE/01.m3u8"
+        }
+    ]
+};
