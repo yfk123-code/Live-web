5b9055846731ed60dcae64ffc517152371306588
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2025-09-23 12:25:39 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,104 +4,103 @@ const tvData = {
             name: "DD News",
             logo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/DD_News.svg/1200px-DD_News.svg.png",
             stream_url: "https://ddnewslive.akamaized.net/hls/live/2021703/ddnews/ddnews.m3u8",
-            category: "News" // <-- Nayi Property
+            category: "News"
         },
         {
             name: "India TV",
             logo: "https://upload.wikimedia.org/wikipedia/commons/f/f4/India-tv-logo.png",
             stream_url: "https://indiatv.live247stream.com/indiatv/live/playlist.m3u8",
-            category: "News" // <-- Nayi Property
+            category: "News"
         },
        {
             name: "B4U Kadak",
             logo: "https://jiotvimages.cdn.jio.com/dare_images/images/B4U_Kadak.png",
             stream_url: "https://cdnb4u.wiseplayout.com/B4U_Kadak/SD216/SD216.m3u8",
-            category: "Movies" // <-- Nayi Property
+            category: "Movies"
         },
         {
             name: "SET HD",
             logo: "https://i.postimg.cc/jC0dMyzk/Sony-HD.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011671%2FSETHD%2Fmaster_900.m3u8",
-            category: "Entertainment" // <-- Nayi Property
+            category: "Entertainment"
         },
         {
             name: "Sony Marathi HD",
             logo: "https://i.postimg.cc/0yZKyHGk/Sony-Marathi-SD.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011740%2FSonyMarathi%2Fmaster_900.m3u8",
-            category: "Entertainment" // <-- Nayi Property
+            category: "Entertainment"
         },
         {
             name: "Sony SAB HD",
             logo: "https://i.postimg.cc/7Pg6LWDF/Sony-SAB-HD.png",
-            stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011749%2FSABHD%2Fmaster_900.m3u8
-",
-            category: "Entertainment" // <-- Nayi Property
+            stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011749%2FSABHD%2Fmaster_900.m3u8",
+            category: "Entertainment"
         },
         {
             name: "Sony Pal HD",
             logo: "https://i.postimg.cc/qMR9FNm1/Sony-Pal.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011741%2FSonyPalSD%2Fmaster_900.m3u8",
-            category: "Entertainment" // <-- Nayi Property
+            category: "Entertainment"
         },
         {
             name: "Sony Yay HD",
             logo: "https://i.postimg.cc/Y0V8ZC9P/Sony-Yay-Hindi.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011746%2FSonyYaySD%2Fmaster_900.m3u8",
-            category: "Kids" // <-- Nayi Property
+            category: "Kids"
         },
         {
             name: "Sony Max HD",
             logo: "https://i.postimg.cc/9fjgc0p5/Sony-Max-HD.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011670%2FSonyMaxhd%2Fmaster_900.m3u8",
-            category: "Movies" // <-- Nayi Property
+            category: "Movies"
         },
         {
             name: "Sony Max",
             logo: "https://i.postimg.cc/NjvkHVrR/SET-MAX.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011745%2FSonyMaxSD%2Fmaster_900.m3u8",
-            category: "Movies" // <-- Nayi Property
+            category: "Movies"
         },
         {
             name: "Sony Max 2",
             logo: "https://i.postimg.cc/pTSDX1Hh/Sony-MAX2.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011908%2FMAX2%2Fmaster_900.m3u8",
-            category: "Movies" // <-- Nayi Property
+            category: "Movies"
         },
         {
             name: "Sony WAH",
             logo: "https://i.postimg.cc/xCX6Rmjm/Sony-Wah.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011906%2FSonyWah%2Fmaster_900.m3u8",
-            category: "Movies" // <-- Nayi Property
+            category: "Movies"
         },
         {
             name: "Sony BBC Earth HD",
             logo: "https://i.postimg.cc/KjXDDsHX/Sony-BBC-Earth-HD.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011907%2FSonyBBCEarthHD%2Fmaster_900.m3u8",
-            category: "Infotainment" // <-- Nayi Property
+            category: "Infotainment"
         },
         {
             name: "Sony Sports Ten 3",
             logo: "https://i.postimg.cc/02Pp5mgC/Ten-3.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2020592%2FTEN3SD%2Fmaster_900.m3u8",
-            category: "Sports" // <-- Nayi Property
+            category: "Sports"
         },
         {
             name: "Sony Sports Ten 3 HD",
             logo: "https://i.postimg.cc/g2BRKTxM/Ten3-HD.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2020591%2FTEN3HD%2Fmaster_900.m3u8",
-            category: "Sports" // <-- Nayi Property
+            category: "Sports"
         },
          {
             name: "Sony Sports Ten 1",
             logo: "https://i.postimg.cc/Wp6ky3Dq/Ten-1.png",
             stream_url: "https://freelivtv.xyz/proxy/m3u8.php?url=https%3A%2F%2Fdish.slivcdn.com%2Fhls%2Flive%2F2011739%2FTEN1SD%2Fmaster_900.m3u8",
-            category: "Sports" // <-- Nayi Property
+            category: "Sports"
         },
        {
             name: "9xm",
             logo: "https://jiotvimages.cdn.jio.com/dare_images/images/9XM.png",
             stream_url: "https://d35j504z0x2vu2.cloudfront.net/v1/manifest/0bc8e8376bd8417a1b6761138aa41c26c7309312/9xm/23886666-8fc5-470f-aab1-bd637ed607b1/3.m3u8",
-            category: "Music" // <-- Nayi Property
+            category: "Music"
         }
    ],
     "USA": [
@@ -109,15 +108,15 @@ const tvData = {
             name: "NASA TV",
             logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png",
             stream_url: "https://uplynkcontent.sinclairstoryline.com/channel/ddd76fdc1c0a456ba5394f71313f5963.m3u8",
-            category: "Entertainment" // <-- Nayi Property
+            category: "Entertainment"
         }
     ],
     "International": [
         {
             name: "Al Jazeera",
-            logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Aljazeera_eng.svg/512px-Aljazeera_eng.svg.png",
+            logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Aljazeera_eng.svg/512px-Aljazeera_eng.png",
             stream_url: "https://live-hls-web-aje.getaj.net/AJE/01.m3u8",
-            category: "News" // <-- Nayi Property
+            category: "News"
         }
     ]
 };
